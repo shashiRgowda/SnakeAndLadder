0e234f1bd1a73502e275fd5ba13639f657ac9df3
shashiRgowda <shashikumartr64@gmail.com>
2021-05-20 13:17:24 +0530

    Added:UC1_PlayerStartingPositionZero

---
+++ UC1_PlayerStartingPositionZero/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SnakeandLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to SnakeandLadder program");
+
+            int position=0;
+            Console.WriteLine("Player is at position : " + position);
+        }
+    }
+}
