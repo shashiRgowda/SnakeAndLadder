fd22280e43bdce389ccf4d9ac385d24dd4169171
shashiRgowda <shashikumartr64@gmail.com>
2021-05-20 13:22:07 +0530

    Added:UC2_PlayerRollsDie

---
+++ UC2_PlayerRollsADie/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace UC2_SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int position = 0;
+            Console.WriteLine("Player is at position: "+position);
+            Random random = new Random();
+            int droll = 0;
+            while (true)
+            {
+                droll = random.Next(7);
+                if (droll != 0) break;
+
+            }
+            Console.WriteLine(droll);
+        }
+    }
+}
